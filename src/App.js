a314761c1d3ad7d5da87b1b3fa707e4562cb1eb1
Damin <svs0371@naver.com>
2020-07-13 14:12:04 +0900

    #2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import PropTypes from "prop-types"
+
+
+// foodIlike.map(dish => (<Food name={dish.name}))
+const foodIlike = [
+  {id:1,rating:5.0,name:"ipad", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/IPad_1_PSD_Mockup.png/1200px-IPad_1_PSD_Mockup.png"},
+  {id:2,rating:4.9,name:"SG", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/IPad_1_PSD_Mockup.png/1200px-IPad_1_PSD_Mockup.png"},
+  
+  {id:3,rating:3.8,name:"air3", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/IPad_1_PSD_Mockup.png/1200px-IPad_1_PSD_Mockup.png"},
+  
+  {id:4,rating:4.3 ,name:"apple", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/IPad_1_PSD_Mockup.png/1200px-IPad_1_PSD_Mockup.png"},
+  
+  {id:5,rating:4.6,name:"pencil", image:"https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/IPad_1_PSD_Mockup.png/1200px-IPad_1_PSD_Mockup.png"}
+]
+
+function renderFood(dish){
+  return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
+
+}
+
+//props {name, picture}
+function Food(props){
+  return <div>
+    <h3> i like {props.name}</h3>
+    <h4>{props.rating}/5.0</h4>
+    <img src={props.picture}></img>
+
+    </div>
+}
+
+Food.propTypes = {
+  name: PropTypes.string.isRequired,
+  picture:PropTypes.string.isRequired,
+  rating: PropTypes.number.isRequired
+
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {foodIlike.map(dish => <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>)}
     </div>
   );
 }
